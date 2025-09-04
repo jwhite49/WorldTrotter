60cca745e9d67380d25cde550e4ffc8ed7da1355
jordan white <jwhite49@jacksonville.edu>
2025-09-04 15:37:24 -0400

    Initial Commit

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Jordan White on 9/4/25.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ConversionViewController loaded its view.")
+        
+    }
+
+
+}
+
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Jordan White on 9/16/25.
+//
+
+import UIKit
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("MapViewController loaded its view.")
+    }
+}
