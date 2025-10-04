826d12596521b3488db884ce49532b9b63491d9b
jordan white <jwhite49@jacksonville.edu>
2025-10-04 01:24:02 -0400

    chpater 6 bronze challenge

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
-    
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("ConversionViewController loaded its view.")
-        
+        textField.delegate = self
     }
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
@@ -34,7 +34,11 @@ class ConversionViewController: UIViewController {
     }
     func updateCelsiusLabel() {
         if let celsiusValue = celsiusValue {
-            celsiusLabel.text = "\(celsiusValue.value)"
+            let formatter = NumberFormatter()
+                   formatter.numberStyle = .decimal
+                   formatter.maximumFractionDigits = 1
+                   formatter.minimumFractionDigits = 1
+                   celsiusLabel.text = formatter.string(from: NSNumber(value: celsiusValue.value))
         }
         else {
             celsiusLabel.text = "???"
@@ -50,7 +54,41 @@ class ConversionViewController: UIViewController {
         }
     }
     
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
     
+    func textField(_ textField: UITextField,
+            shouldChangeCharactersIn range: NSRange,
+            replacementString string: String) -> Bool {
+
+        if string.isEmpty { return true }
+
+        let allowedCharacters = CharacterSet.decimalDigits.union(CharacterSet(charactersIn: "."))
+           
+        if string.rangeOfCharacter(from: allowedCharacters.inverted) != nil {
+            return false
+        }
+
+        let currentText = textField.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+
+        let decimalSeparator = "."
+        let decimalCount = updatedText.components(separatedBy: decimalSeparator).count - 1
+        if decimalCount > 1 {
+            return false
+        }
+
+        if let dotIndex = updatedText.firstIndex(of: ".") {
+            let decimals = updatedText[updatedText.index(after: dotIndex)...]
+            if decimals.count > 1 {
+                return false
+            }
+        }
+
+        return true
+    }
 
 
 }
